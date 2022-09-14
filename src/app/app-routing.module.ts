ac45cb395844e9560c138366a8bd02d0d7d7aa0d
Jordan <jordant710@gmail.com>
2022-09-14 10:51:12 -0400

    adding a login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent} from './app.component';
+import { LoginComponent } from './Login Screen Components/login/login.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashbaord' },
-  { path: 'dashboard', component: AppComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'homepage' },
+  { path: 'homepage', component: AppComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Dashboard Contents/header/header.component';
 import { CarouselComponent } from './Dashboard Contents/carousel/carousel.component';
 import { CardsComponent } from './Dashboard Contents/cards/cards.component';
+import { LoginComponent } from './Login Screen Components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarouselComponent,
-    CardsComponent
+    CardsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
